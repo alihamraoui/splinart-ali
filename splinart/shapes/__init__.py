0040ce6b214d72c3ffca19d747f621ed053ff2dd
Ali Hamraoui <hamraoui@bio.ens.psl.eu>
2024-07-03 13:40:05 +0200

    initial commit

---
+++ splinart/shapes/__init__.py
@@ -0,0 +1,8 @@
+# Author:
+#     Loic Gouarin <loic.gouarin@gmail.com>
+#
+# License: BSD 3 clause
+"""Shape package."""
+from .base import line, circle
+
+__all__ = ["line", "circle"]
---
+++ splinart/spline/__init__.py
@@ -0,0 +1,9 @@
+# Author:
+#     Loic Gouarin <loic.gouarin@gmail.com>
+#
+# License: BSD 3 clause
+"""Spline package."""
+from .spline import spline
+from .splint import splint
+
+__all__ = ["spline", "splint"]
