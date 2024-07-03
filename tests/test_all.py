25cac0595dafa69096c7706856c1b04e5d0d76ed
Hamraoui Ali <72599648+alihamraoui@users.noreply.github.com>
2024-07-03 14:50:20 +0200

    Add precommit (#2)

--- tests/test_all.py
+++ tests/test_all.py
@@ -3,10 +3,10 @@ import numpy as np
 import pytest
 import matplotlib.pyplot as plt
 
+
 @pytest.mark.long
 @pytest.mark.mpl_image_compare
 def test_circle_case():
-
     np.random.seed(42)
 
     img_size, channels = 1000, 4
@@ -16,11 +16,13 @@ def test_circle_case():
 
     def xs_func():
         nsamples = 500
-        return (np.random.random() + 2 * np.pi * np.linspace(0, 1, nsamples))%(2*np.pi)
+        return (np.random.random() + 2 * np.pi * np.linspace(0, 1, nsamples)) % (
+            2 * np.pi
+        )
 
     spl.update_img(img, path, xs_func, nrep=4000, x=theta)
 
     fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
+    ax = fig.add_subplot(1, 1, 1)
     ax.imshow(img)
     return fig
